bb5ae26572557c8ecbfef34fca10f954ddd19d2b
Elizeu <loopsdesenvolvedor@gmail.com>
2025-11-05 02:37:09 -0300

    feat: add initial Prisma configuration

---
+++ src/lib/prisma.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@/generated/prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
