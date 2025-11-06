a38181be17defe2e6c053b72729a39cde397b200
Elizeu <loopsdesenvolvedor@gmail.com>
2025-11-06 19:27:07 -0300

    feat(auth): add authentication setup with Google and Nodemailer

---
+++ src/lib/auth.ts
@@ -0,0 +1,38 @@
+import NextAuth from "next-auth";
+import Nodemailer from "next-auth/providers/nodemailer";
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "./prisma";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Nodemailer({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+    Google({
+      clientId: "",
+      clientSecret: "",
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+    newUser: "/auth/signup",
+    error: "/auth/error",
+  },
+
+  callbacks: {
+    async session({ session, user, token }) {
+      return session;
+    },
+  },
+});
